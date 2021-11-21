cf615e6f22d8e9194402682e781bbccc6ed4a180
Vinicius Zapalá dos Santos <40218275+ViniZap4@users.noreply.github.com>
2021-11-21 16:49:42 -0300

    update set level scene

--- scenes/SetLevel/SetLevel.gd
+++ scenes/SetLevel/SetLevel.gd
@@ -3,29 +3,61 @@ extends Control
 var focusAnimate
 
 func _ready():
-	$GridContainer/LineOption/Back.grab_focus()
+	$GridContainer/VBoxContainer/Levls/level1.grab_focus()
 	$Panel/AnimationPlayer.current_animation = "mov"
-	$GridContainer/LineOption/AnimationBackButtom.current_animation = "movFont"
-	$GridContainer/LineOption/Back/AudioStreamPlayer.play()
+	$GridContainer/VBoxContainer/Levls/level1/AnimationPlayer.current_animation = "movButton"
+	$GridContainer/VBoxContainer/Levls/level1/AudioStreamPlayer.play()
 	focusAnimate = "Back"
 
 
 func _process(delta):
 
+#level 1
+
+	if $GridContainer/VBoxContainer/Levls/level1.is_hovered():
+		if focusAnimate != "Level1" and focusAnimate == "none" :
+			$GridContainer/VBoxContainer/Levls/level1/AnimationPlayer.current_animation = "movButton"
+			$GridContainer/VBoxContainer/Levls/level1/AudioStreamPlayer.play()
+			focusAnimate = "Level1"
+
+	else:
+		if focusAnimate == "Level1" :
+			$GridContainer/VBoxContainer/Levls/level1/AnimationPlayer.current_animation  = "movButtonBack"
+			focusAnimate = "none"
+
+#level 2
+
+	if $GridContainer/VBoxContainer/Levls/level2.is_hovered():
+		if focusAnimate != "Level2" and focusAnimate == "none" :
+			$GridContainer/VBoxContainer/Levls/level2/AnimationPlayer.current_animation = "movButton"
+			$GridContainer/VBoxContainer/Levls/level2/AudioStreamPlayer.play()
+			focusAnimate = "Level2"
+
+	else:
+		if focusAnimate == "Level2" :
+			$GridContainer/VBoxContainer/Levls/level2/AnimationPlayer.current_animation  = "movButtonBack"
+			focusAnimate = "none"
+
+
+
 # Back
 
-	if $GridContainer/LineOption/Back.is_hovered():
+	if $GridContainer/VBoxContainer/LineOption/Back.is_hovered():
 		if focusAnimate != "Back" and focusAnimate == "none" :
-			$GridContainer/LineOption/AnimationBackButtom.current_animation = "movFont"
-			$GridContainer/LineOption/Back/AudioStreamPlayer.play()
+			$GridContainer/VBoxContainer/LineOption/AnimationBackButtom.current_animation = "movFont"
+			$GridContainer/VBoxContainer/LineOption/Back/AudioStreamPlayer.play()
 			focusAnimate = "Back"
 
 	else:
 		if focusAnimate == "Back" :
-			$GridContainer/LineOption/AnimationBackButtom.current_animation = "movFontBack"
+			$GridContainer/VBoxContainer/LineOption/AnimationBackButtom.current_animation = "movFontBack"
 			focusAnimate = "none"
 
 
 func _on_Back_pressed():
 	get_tree().change_scene("res://scenes/MainMenu/MainMenu.tscn")
 
+
+
+func _on_level1_pressed():
+	get_tree().change_scene("res://scenes/Levels/Level1.tscn")
