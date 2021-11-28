658e989385b5bb4913af1290e06eca208071a7c5
Vinicius Zapalá dos Santos <40218275+ViniZap4@users.noreply.github.com>
2021-11-27 21:37:02 -0300

    update datas

--- components/globals/global.gd
+++ components/globals/global.gd
@@ -11,20 +11,18 @@ var life = 3
 
 var blocksInLevel=0
 
-
 var state = ""
 
 var NameGame = "VPBreakout"
 var apiKey = ""
 var version =  "1.0.0"
 
-
 func _ready():
 	var file = File.new()
 	file.open('res://apiKey.env', File.READ)
 	self.apiKey=file.get_line()
 	file.close() 
-	
+
 	SilentWolf.configure({
 		"api_key": self.apiKey,
 		"game_id": NameGame,
