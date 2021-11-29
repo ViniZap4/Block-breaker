50866026134df2681bcd664de92e39c3c8c9d7fc
Vinicius Zapalá dos Santos <40218275+ViniZap4@users.noreply.github.com>
2021-11-29 20:21:52 -0300

    update fullscreen component

--- components/fullScreen/fullScreen.gd
+++ components/fullScreen/fullScreen.gd
@@ -8,7 +8,13 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if OS.window_fullscreen:
+		$fullScreen.hide()
+		$NormalScreen.show()
+	else:
+		$fullScreen.show()
+		$NormalScreen.hide()
+	
 
 
 
